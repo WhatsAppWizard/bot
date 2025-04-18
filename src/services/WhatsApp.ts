b0c7234c3a392856463be7fd656db9d8f732113b
Mahmoud Nasr <239.nasr@gmail.com>
2025-04-18 10:46:51 +0200

    feat: Add bot command setup for user interactions and response handling

--- src/services/WhatsApp.ts
+++ src/services/WhatsApp.ts
@@ -149,7 +149,36 @@ class WhatsApp {
       await user.block();
     });
   }
+  private async setupBotCommands(message: Message) {
 
+    const command = message.body.toLowerCase().trim();
+
+    const commands = [
+      "hi whatsapp wizard",
+      "hi",
+      "hello",
+      "hey",
+      "whatsapp wizard",
+      "wwz",
+      "wwz bot",
+      "wwz bot help",
+      ".",
+      "help",
+      "commands",
+      "start",
+      "stickers"
+    ]
+
+    if (commands.includes(command)) {
+      message.reply(`
+        Hi there! I'm WhatsApp Wizard.
+        Now you can send my any Link from Facebook, Tiktok, Instagram, YouTube or Twitter and I will download it for you.\n
+        Also, We still supporting make stickers from images! Send me any image and I will make a sticker for you.`);
+     
+    }
+
+   
+  }
   private setupMessageHandler() {
     // Listen for new messages
     this.client.on("message", async (message) => {
@@ -159,6 +188,7 @@ class WhatsApp {
           // Ignore group messages and read-only chats
           return;
         }
+        await this.setupBotCommands(message);
         const { body, timestamp, hasMedia, links } = message;
         const contactInfo = await message.getContact();
         const userNumber = await contactInfo.getFormattedNumber();
