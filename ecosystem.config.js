a5854a599bf5168f54c28a06ecafce925575a6c6
Mahmoud Nasr <239.nasr@gmail.com>
2025-04-21 11:35:20 +0200

    fix: update build script names and enhance error handling in index.ts

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [{
+      name: "wwz",
+      script: "./build/index.js",
+      watch: true,
+      max_restarts: 10,
+      restart_delay: 5000, 
+      autorestart: true,
+      out_file: "./logs/out.log",
+      error_file: "./logs/error.log"
+    }]
+  }
+  
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import TelegramService from "./services/Telegram";
-import WhatsApp from "./services/WhatsApp";
-import app from "./services/Express";
 import dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
+import app from "./services/Express";
+import TelegramService from "./services/Telegram";
+import WhatsApp from "./services/WhatsApp";
 
 dotenv.config();
 
 
 const telegramService = TelegramService.getInstance();
-
+setupExitHandlers();
 async function main() {
   const publicDir = path.join(process.cwd(), "public");
   if (!fs.existsSync(publicDir)) {
@@ -19,7 +19,7 @@ async function main() {
   const whatsapp = new WhatsApp();
 
   // Set up exit handlers
-  setupExitHandlers();
+
 
   // Make WhatsApp instance accessible to routes
   app.set("whatsapp", whatsapp);
@@ -31,20 +31,30 @@ function setupExitHandlers() {
   // Handle graceful shutdown
   process.on("SIGINT", () => {
     telegramService.sendMessage("Server is shutting down gracefully...");
+    process.exit(1); 
+
   });
   process.on("SIGTERM", () => {
     telegramService.sendMessage("MAYDAY: SERVER CRASHED...");
+    process.exit(1); 
+
   });
 
   // Handle uncaught exceptions
   process.on("uncaughtException", (error) => {
     console.error("Uncaught Exception:", error);
+    telegramService.sendMessage("Another Unhandled Exception" + error);
+
+    process.exit(1);
     
   });
 
   // Handle unhandled promise rejections
   process.on("unhandledRejection", (reason, promise) => {
     console.error("Unhandled Rejection at:", promise, "reason:", reason);
+    telegramService.sendMessage("Another Unhandled Rejection" + reason);
+
+    process.exit(1);
 
   });
 }
