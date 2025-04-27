8d5f21c07537f1399f96cd73ae5028c958092223
Mahmoud Nasr <239.nasr@gmail.com>
2025-04-27 20:19:41 +0300

    fix: pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-    apps: [{
+  apps: [
+    {
       name: "wwz",
       script: "./build/index.js",
-      watch: true,
+      watch: false,
       max_restarts: 10,
-      restart_delay: 5000, 
+      restart_delay: 5000,
       autorestart: true,
       out_file: "./logs/out.log",
-      error_file: "./logs/error.log"
-    }]
-  }
-  
+      error_file: "./logs/error.log",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
