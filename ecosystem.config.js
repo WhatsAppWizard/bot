91d038c91856c0813b1bcc0036632838b1612712
Mahmoud Nasr <239.nasr@gmail.com>
2025-09-03 11:53:46 +0300

    Comment out cron_restart setting in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       max_restarts: 10,
       restart_delay: 5000,
       autorestart: true,
-      cron_restart: '0 */3 * * *',
+      //cron_restart: '0 */3 * * *',
       out_file: "./logs/out.log",
       error_file: "./logs/error.log",
       env: {
